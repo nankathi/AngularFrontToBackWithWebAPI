1f1b38c191b12b7164d62d9d5d1c911568c42ca6
Raj <Raj@LASCBASEW7>
2016-06-16 16:14:07 -0700

    Add project files.

---
+++ APM.WebAPI/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APM.WebAPI.Models
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Productcode { get; set; }
+    }
+}
