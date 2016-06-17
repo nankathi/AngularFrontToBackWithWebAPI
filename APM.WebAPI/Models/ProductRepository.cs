9f03fb5c310d9ef9419dbc21e287eb2e0eb96ad2
nankathi <dotnethelp4u@gmail.com>
2016-06-16 22:44:55 -0700

    Added .json file, product class, product repository class

---
+++ APM.WebAPI/Models/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APM.WebAPI.Models
+{
+    /// <summary>
+    /// Stores the data in a json file so that no database is required for this
+    /// sample application
+    /// </summary>
+    public class ProductRepository
+    {
+        ///<summary>
+        ///Creates a new product with default vlaues
+        ///</summary>
+        ///<return></returns>
+        internal Product Create()
+        {
+            Product product = new Product
+            {
+                ReleaseDate = DateTime.Now
+            };
+            return product;
+        }
+        
+
+    }
+}
